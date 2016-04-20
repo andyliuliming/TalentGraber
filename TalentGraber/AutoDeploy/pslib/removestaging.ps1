4f23e446900651d5f101a0bb126f0534b942ea8c
andyliuliming <andyliuliming@outlook.com>
2016-04-20 16:13:54 +0800

    talent graber.

---
+++ TalentGraber/AutoDeploy/pslib/removestaging.ps1
@@ -0,0 +1,17 @@
+﻿if($Environment -eq "AzureChinaCloud")
+{
+. .\pslib\common-china.ps1
+}else
+{
+. .\pslib\common-global.ps1
+}
+. .\pslib\azureadapter.ps1
+
+
+$ErrorActionPreference= 'Continue'
+
+
+$Slot="Staging"
+
+Remove-AzureDeployment -ServiceName $ProductAzureServiceName -Slot $Slot -Force
+Write-Host "$ProductAzureServiceName is removed"
---
+++ TalentGraber/ServiceBase/GoldODataController.cs
@@ -0,0 +1,19 @@
+﻿using Macrodeek.Model;
+using System;
+using System.Diagnostics;
+
+namespace Macrodeek.StarDustServiceBase.Controllers
+{
+    public class GoldODataController : ODataBaseController
+    {
+        protected GoldModelContainer db = new GoldModelContainer();
+        public GoldODataController()
+            : base()
+        {
+            db.Database.Log = new Action<string>(str =>
+            {
+                Trace.TraceInformation(str);
+            });
+        }
+    }
+}
